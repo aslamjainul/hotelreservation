410981b3d8efff63d2ba0cdd0f031724959ecf0e
jainulaslam.m <jainulaslam.m@zohocorp.com>
2017-05-18 12:10:50 +0530

    Request package..

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var config = require('config.json');
 
 router.get('/', function (req, res) {
     res.render('register');
@@ -10,7 +9,7 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     // register using api to maintain clean separation between layers
     request.post({
-        url: config.apiUrl + '/users/register',
+        url: '/users/register',
         form: req.body,
         json: true
     }, function (error, response, body) {
