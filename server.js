13eb56c6ff594e96957bbe05d6f411a78d3e9176
jainulaslam.m <jainulaslam.m@zohocorp.com>
2017-05-18 16:25:51 +0530

    fix -- using body parser

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-app.use(express.bodyParser());
+app.use(bodyParser());
 app.use(cookieParser());
 app.use(session({secret: "CodaLoginSecret"}));
 
